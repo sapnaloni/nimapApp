65473d60f0e4df7a704557d497412f6dcabc270c
sapnaloni <147479523+sapnaloni@users.noreply.github.com>
2024-03-19 11:40:27 +0530

    make changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,11 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent
+{
+  loginForm=new FormGroup({
+    email:new FormControl('',Validators.compose([Validators.required,Validators.email])),
+    password:new FormControl('',Validators.compose([Validators.required,Validators.minLength(6),Validators.maxLength(10)])),
+  })
+formSubmitted=false
+proceedLogin()
+{
+  this.formSubmitted=true
 
+  if(this.loginForm.valid)
+  {
+    alert("Login successfull")
+  }
+}
 }
